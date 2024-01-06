5c555f3c47323a53ffee4119ab15d76d8e5f03fa
yonatanbeery <yonatanbeery@gmail.com>
2024-01-06 17:56:06 +0200

    has nav bar between profile and posts

--- app/src/main/java/com/example/homefinder/MainActivity.kt
+++ app/src/main/java/com/example/homefinder/MainActivity.kt
@@ -36,14 +36,14 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(arraySetOf(R.id.nav_home, R.id.nav_gallery, R.id.nav_logout), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(arraySetOf(R.id.nav_home, R.id.nav_profile, R.id.nav_logout), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.posts, menu)
+        //menuInflater.inflate(R.menu.posts, menu)
         return true
     }
 
