96d3f020d90e5ac5e92a6496c849fead137e4266
yonatanbeery <yonatanbeery@gmail.com>
2024-01-20 00:15:49 +0200

    showing list with recyclerView (better then list view)

--- app/src/main/java/com/example/yournexthome/Posts.kt
+++ app/src/main/java/com/example/yournexthome/Posts.kt
@@ -6,67 +6,82 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
 import android.widget.CheckBox
-import android.widget.ListView
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.yournexthome.Model.Model
 import com.example.yournexthome.Model.Post
 
 class Posts : AppCompatActivity() {
-    private var postsListView: ListView? = null
+    private var postsRecyclerView: RecyclerView? = null
     private var posts: MutableList<Post>? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.posts)
+        setContentView(R.layout.posts_list)
 
         posts = Model.instance.posts
 
-        postsListView = findViewById(R.id.postsList)
-        postsListView?.adapter = PostsListAdapter(posts)
+        postsRecyclerView = findViewById(R.id.postsList)
+        postsRecyclerView?.setHasFixedSize(true)
+        postsRecyclerView?.layoutManager = LinearLayoutManager(this)
 
-        postsListView?.setOnItemClickListener { parent, view, position, id ->
-            Log.i("Tag","Row $position was clicked" )
+        val adapter = PostsRecyclerAdapter()
+        adapter.listener = object : OnItemClickListener {
+            override fun onItemClick(position: Int) {
+                Log.i("Tag", "row $position")
+            }
         }
+        postsRecyclerView?.adapter = adapter
     }
 
-    class PostsListAdapter(val posts: MutableList<Post>?): BaseAdapter() {
-        override fun getCount(): Int = posts?.size ?: 0
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+    }
 
-        override fun getItem(p0: Int): Any {
-            TODO("Not yet implemented")
-        }
+    inner class PostViewHolder(itemView: View, listener: OnItemClickListener?): RecyclerView.ViewHolder(itemView){
 
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-            val post = posts?.get(position)
-            var view: View? = null
-            if (convertView == null){
-                view = LayoutInflater.from(parent?.context).inflate(R.layout.post_list_row,parent,false)
-                val postCheckBox:CheckBox? = view?.findViewById(R.id.postRowCheckbox)
-                postCheckBox?.setOnClickListener {
-                    (postCheckBox.tag as? Int)?.let { tag ->
-                        posts?.get(tag)?.isChecked = postCheckBox.isChecked
-                    }
+        var postHeaderText: TextView? = null
+        var postDescription: TextView?= null
+        var postCheckBox: CheckBox?= null
+        init {
+             postHeaderText = itemView.findViewById(R.id.postHeader)
+             postDescription= itemView.findViewById(R.id.postDescription)
+             postCheckBox = itemView.findViewById(R.id.postRowCheckbox)
+
+            postCheckBox!!.setOnClickListener {
+                (postCheckBox!!.tag as? Int)?.let { tag ->
+                    posts?.get(tag)?.isChecked = postCheckBox!!.isChecked
                 }
             }
+            itemView.setOnClickListener {
+                Log.i("Tag", "row adapter $adapterPosition")
+                listener?.onItemClick(adapterPosition)
+            }
+        }
 
-            view = view ?: convertView
-            val postHeaderText: TextView? = view?.findViewById(R.id.postHeader)
-            val postDescription: TextView? = view?.findViewById(R.id.postDescription)
-            val postCheckBox: CheckBox? = view?.findViewById(R.id.postRowCheckbox)
+        fun bind(post: Post?, position: Int) {
             postHeaderText?.text = post?.header
             postDescription?.text = post?.description
             postCheckBox?.apply {
                 isChecked = post?.isChecked ?: false
-                    tag = position
+                tag = position
             }
-
-            return view!!
+        }
+    }
+    inner class PostsRecyclerAdapter: RecyclerView.Adapter<PostViewHolder>() {
+        var listener:OnItemClickListener? = null
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+            Log.i("JOHN", "$posts")
+            return PostViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.post_list_row,parent,false), listener)
         }
 
-        override fun getItemId(p0: Int): Long {
-            TODO("Not yet implemented")
+        override fun getItemCount(): Int = posts?.size ?: 0
+
+        override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+            val post = posts?.get(position)
+            holder.bind(post, position)
         }
+
     }
 }
