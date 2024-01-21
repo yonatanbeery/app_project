5e8dac6343b685705bd403a6d97c749882e39c09
yonatanbeery <yonatanbeery@gmail.com>
2024-01-21 23:52:28 +0200

    menu to navbar

--- app/src/main/java/com/example/yournexthome/MainActivity.kt
+++ app/src/main/java/com/example/yournexthome/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.yournexthome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+    private var navController: NavController? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment: NavHostFragment? = supportFragmentManager.findFragmentById(R.id.nav_host_main) as? NavHostFragment
+        navController = navHostFragment?.navController
+        navController?.let {
+            NavigationUI.setupActionBarWithNavController(this, it)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                navController?.navigateUp()
+                true
+            }
+            R.id.mibtnLogout -> {
+                //todo logout
+                true
+            }
+            else -> navController?.let{ NavigationUI.onNavDestinationSelected(item, it)} ?: false
+        }
     }
 }
