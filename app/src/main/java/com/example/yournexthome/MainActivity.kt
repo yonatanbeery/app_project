2eab086f5dc8cd71fca5216a180062cfac3178ac
yonatanbeery <yonatanbeery@gmail.com>
2024-02-03 19:13:39 +0200

    login and register done

--- app/src/main/java/com/example/yournexthome/MainActivity.kt
+++ app/src/main/java/com/example/yournexthome/MainActivity.kt
@@ -9,18 +9,27 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.google.firebase.firestore.firestore
 
 class MainActivity : AppCompatActivity() {
     private var navController: NavController? = null
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
         setContentView(R.layout.activity_main)
 
         val navHostFragment: NavHostFragment? = supportFragmentManager.findFragmentById(R.id.nav_host_main) as? NavHostFragment
         navController = navHostFragment?.navController
         navController?.let {
             NavigationUI.setupActionBarWithNavController(this, it)
+        }
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            navController?.navigate(R.id.action_global_postsFragment)
         }
     }
 
@@ -31,16 +40,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val currentUser = auth.currentUser
+
         return when (item.itemId) {
             android.R.id.home -> {
                 navController?.navigateUp()
                 true
             }
             R.id.mibtnLogout -> {
-                //todo logout
+                auth.signOut()
+                navController?.navigateUp()
                 true
             }
-            else -> navController?.let{ NavigationUI.onNavDestinationSelected(item, it)} ?: false
+            else -> {
+                if (currentUser != null) {
+                    navController?.let{ NavigationUI.onNavDestinationSelected(item, it)} ?: false
+                }
+                true
+            }
         }
     }
 }
