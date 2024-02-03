2b10c21ad306387324d914669565aef7bb3c3dec
yonatanbeery <yonatanbeery@gmail.com>
2024-02-03 19:35:53 +0200

    blank user settings page

--- app/src/main/java/com/example/yournexthome/user/ProfileFragment.kt
+++ app/src/main/java/com/example/yournexthome/user/ProfileFragment.kt
@@ -1,60 +1,49 @@
 package com.example.yournexthome.user
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.Navigation
 import com.example.yournexthome.R
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var usernameTextView: TextView? = null
+    private var emailTextView: TextView? = null
+    private var passwordTextView: TextView? = null
+    private var confirmPasswordTextView: TextView? = null
+    private var errorMessageTextView: TextView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        initProfileValues(view)
+        val registerBtn: Button = view.findViewById(R.id.update_btn)
+        registerBtn.setOnClickListener(::onUpdateUserButtonClicked)
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    fun initProfileValues(view: View) {
+        usernameTextView = view.findViewById(R.id.newUsername)
+        emailTextView = view.findViewById(R.id.newEmail)
+        passwordTextView = view.findViewById(R.id.newPassword)
+        confirmPasswordTextView = view.findViewById(R.id.confirmNewPassword)
+        errorMessageTextView = view.findViewById(R.id.errorMessage)
+
+        val user = Firebase.auth.currentUser
+        usernameTextView?.text = user?.email
+    }
+
+    fun onUpdateUserButtonClicked(view: View) {
+        //todo update user settings
     }
 }
